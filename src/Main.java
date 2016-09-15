dda27d8d2e545988e713a59abb3c6c207c756425
RhullSTG <rachael.hull@stgconsulting.com>
2016-09-15 12:23:23 -0600

    Third Commit. Added polling.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 import java.util.Calendar;
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.mail.*;
 
 public class Main {
 
+    public static String ip = "10.1.10.105";
+
     public static void main(String[] args) {
 
+        Timer timer = new Timer();
+        long interval = (5*60*1000); //polls for 5 mins
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runDiscoBall();
+            }
+        }, 0, interval);
+
+    }
+
+    public static void runDiscoBall(){
         Properties props = new Properties();
         props.setProperty("mail.store.protocol", "imaps");
 
@@ -23,43 +40,42 @@ public class Main {
             System.out.println("Subject: " + msg.getSubject());
             System.out.println("Content: " + msg.getContent());
 
-            long startTime = System.currentTimeMillis();
-            long maxDurationInMilliseconds = 30 * 60 * 1000;
-            long totalDuration = startTime + maxDurationInMilliseconds;
-
-//            Calendar calendar = Calendar.getInstance();
-//            calendar.add(Calendar.MINUTE, 5);
-//            long stopPollingTime = calendar.getTimeInMillis();
+//            long startTime = System.currentTimeMillis();
+//            long maxDurationInMilliseconds = 30 * 60 * 1000; //
+//            long totalDuration = startTime + maxDurationInMilliseconds;
 
             //Run for 30 Minutes when the reader finds a recent email
-            while(System.currentTimeMillis() < totalDuration){
-                if(msg.isSet(Flags.Flag.RECENT)) {
-                    WemoDevice wd = new WemoDevice("http://10.1.10.105:49153/setup.xml");
-                    wd.turnOn();
-                    try {
-                        Thread.sleep(100);
-                        wd.turnOff();
-                    } catch (InterruptedException e) {
+//            while(System.currentTimeMillis() < totalDuration){
+//                if(msg.isSet(Flags.Flag.RECENT)) {
+//                    //WemoDevice wd = new WemoDevice("http://10.1.10.105:49153/setup.xml");
+//                    //wd.turnOn();
+//
+//                }
+//
+//            }
+            Timer timer = new Timer();
+            long interval = (2*60*1000);
+            if(msg.isSet(Flags.Flag.RECENT)) {
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        //WemoDevice wd = new WemoDevice("http://"+ ip + "/setup.xml");
+                        System.out.println("Wemo is turned on");
+                        try {
+                            Thread.sleep(5000);
+                            //wd.turnOff();
+                        } catch (InterruptedException e) {
+                            System.out.println("Sleep was interrupted. Could possibly be from connection timeout at: " + ip);
+                        }
                     }
-                }
-
+                },0, interval);
             }
-
             msg.setFlag(Flags.Flag.DELETED, true);
 
         } catch (Exception ex) {
-            ex.printStackTrace();
+            System.out.println("No recent messages found in inbox: stgnewhirediscoball@stgconsulting.com");
+            System.out.println("Wemo is turned off");
+
         }
-
     }
-
-//    public void turnOnWemo(){
-//        WemoDevice wd = new WemoDevice("http://10.1.10.100:49153/setup.xml");
-//        wd.turnOn();
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
