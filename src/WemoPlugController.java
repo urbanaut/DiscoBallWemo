995a9189ba04b723e32ee748b16edd22943dbb9e
RhullSTG <rachael.hull@stgconsulting.com>
2016-09-30 17:07:50 -0600

    added JSON parsing

--- src/WemoPlugController.java
+++ src/WemoPlugController.java
@@ -2,6 +2,16 @@ import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import javax.mail.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.*;
+
 
 public class WemoPlugController {
 
@@ -23,11 +33,35 @@ public class WemoPlugController {
         this.mailBoxPassword = mailBoxPassword;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, JSONException  {
+        
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet("http://localhost:50625/api/DeviceConfigs/1");
+            HttpResponse response = client.execute(request);
+            BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                System.out.println(line);
+                String str = line;
+                JSONObject obj = new JSONObject(str);
+                String deviceName = obj.getString("deviceName");
+                String ipAddress = obj.getString("ipAddress");
+                String runDuration = obj.getString("runDuration");
+                String emailAddress = obj.getString("emailAddress");
+                String emailPassword = obj.getString("emailPassword");
+                System.out.println(deviceName + "\n"
+                        + ipAddress + "\n"
+                        + runDuration + "\n"
+                        + emailAddress + "\n"
+                        + emailPassword + "\n"
+                );
 
-        WemoPlugController wpc = new WemoPlugController(args[0], args[1], args[2], args[3], args[4]);
-        //System.out.println(wpc.toString());
-        wpc.runController();
+                WemoPlugController wpc = new WemoPlugController(ipAddress, deviceName, runDuration, emailAddress, emailPassword);
+                //System.out.println(wpc.toString());
+                wpc.runController();
+            }
+
+
     }
 
     private void runController(){
